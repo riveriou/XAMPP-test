6715a0c8a80be2dad83e8d8169e3397f9819ba4b
riveriou <riverriou@yahoo.com.tw>
2020-07-18 00:47:23 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,47 @@
+<?php
+$serverName = "192.168.100.3";
+$connectionOptions = array(
+    "database" => "ERP",
+    "uid" => "sa",
+    "pwd" => "sasa"
+);
+
+// Establishes the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if ($conn === false) {
+    die(formatErrors(sqlsrv_errors()));
+}
+
+// Select Query
+$tsql = "SELECT @@Version AS SQL_VERSION";
+
+// Executes the query
+$stmt = sqlsrv_query($conn, $tsql);
+
+// Error handling
+if ($stmt === false) {
+    die(formatErrors(sqlsrv_errors()));
+}
+?>
+
+<h1> Results : </h1>
+
+<?php
+while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+    echo $row['SQL_VERSION'] . PHP_EOL;
+}
+
+sqlsrv_free_stmt($stmt);
+sqlsrv_close($conn);
+
+function formatErrors($errors)
+{
+    // Display errors
+    echo "Error information: <br/>";
+    foreach ($errors as $error) {
+        echo "SQLSTATE: ". $error['SQLSTATE'] . "<br/>";
+        echo "Code: ". $error['code'] . "<br/>";
+        echo "Message: ". $error['message'] . "<br/>";
+    }
+}
+?>
