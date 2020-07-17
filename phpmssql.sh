e3585a6db55041aaf5263d4a8bdd2f571ab8f1d3
riveriou <riverriou@yahoo.com.tw>
2020-07-17 23:56:03 +0800

    Create phpmssql.sh

---
+++ phpmssql.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y curl wget vim dialog software-properties-common
+
+
+add-apt-repository ppa:ondrej/php -y
+apt-get update
+apt-get install php7.4 php7.4-dev php7.4-xml -y --allow-unauthenticated
+
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
+#Download appropriate package for the OS version
+#Choose only ONE of the following, corresponding to your OS version
+
+#Ubuntu 20.04
+curl https://packages.microsoft.com/config/ubuntu/19.10/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17
+# optional: for bcp and sqlcmd
+ACCEPT_EULA=Y apt-get install -y mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+apt-get install -y unixodbc-dev
+
+pecl install sqlsrv
+pecl install pdo_sqlsrv
+
+printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/7.4/mods-available/sqlsrv.ini
+printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/7.4/mods-available/pdo_sqlsrv.ini
+
+phpenmod sqlsrv pdo_sqlsrv
+
+apt-get install libapache2-mod-php7.4 apache2
+a2dismod mpm_event
+a2enmod mpm_prefork
+a2enmod php7.4
